4ce7a07ffe1c14b659b4cc8b926e14b309250cf3
deeprathod97 <deeprathod9797@gmail.com>
2024-04-30 12:31:05 +0530

    Java

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript String Methods
+let name = "Deep Rathod";
+console.log(name.length)
+
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2, 4))
+console.log(name.slice(2))
+console.log(name.replace("de", "pe"))
+
+let fr = "Raman" + "Shivika" + "Deep"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+// fr[4]= "o" // This is not possible 
+console.log(fr)
+// QuickQuiz: Use a for loop to print a string
